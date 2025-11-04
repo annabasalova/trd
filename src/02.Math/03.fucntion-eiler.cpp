591d04c839caf43452c2c930531bfde3c1b8708b
annabasalova <101504408+annabasalova@users.noreply.github.com>
2025-11-04 21:32:44 +0300

    Update 03.fucntion-eiler.cpp

--- src/02.Math/03.fucntion-eiler.cpp
+++ src/02.Math/03.fucntion-eiler.cpp
@@ -1,3 +1,5 @@
+// for one
+
 int func(int n){
     int ans = n;
     for(int i=2;i*i<=n;i++){
@@ -9,6 +11,37 @@ int func(int n){
         }
     }
     if(n > 1)ans -=ans/(n);
+    return ans;
+}
+
+//for many
+vi p;
+vi d;
+ 
+void er(int n){
+    d.assign(n+1,0);
+    for(int i=2;i<=n;i++){
+        if(!d[i]){
+            p.pb(i);
+            d[i] = i;
+            for(int j=i*i;j<=n;j+=i)d[j] = i;
+        }
+    }
+}
+ 
+vi f;
+ 
+int func(int n){
+    int nn = n;
+    int x = d[n];
+    int ans = x-1;
+    n/=x;
+    while(n%x == 0){
+        n/=x;
+        ans *=(x);
+    }
+    ans *= f[n];
+    f[nn] = ans;
     return ans;
 }
 
